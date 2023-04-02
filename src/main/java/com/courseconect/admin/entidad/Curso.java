cc470ee4f3338acf4765f39246cdc735ab28655f
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2023-04-02 13:59:04 -0500

    se generan Id,columns y las relaciones en Curso

--- src/main/java/com/courseconect/admin/entidad/Curso.java
+++ src/main/java/com/courseconect/admin/entidad/Curso.java
@@ -3,39 +3,71 @@ package com.courseconect.admin.entidad;
 import lombok.Data;
 import java.util.*;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "cursos")
 public class Curso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "curso_id", nullable = false)
 	private Long cursoId;
-	private String CursoNombre;
+
+	@Basic
+	@Column(name = "curso_nombre", nullable = false, length = 45)
+	private String cursoNombre;
+
+	@Basic
+	@Column(name = "curso_duracion", nullable = false, length = 45)
 	private String cursoDuracion;
+
+	@Basic
+	@Column(name = "curso_descripcion", nullable = false, length = 45)
 	private String cursoDescripcion;
-	
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "instructor_id", referencedColumnName = "instructor_id", nullable = false)
 	private Instructor instructor;
-	
+
+	@ManyToMany
+	@JoinTable(name = "matriculado_en", joinColumns = { @JoinColumn(name = "curso_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "estudiante_id") })
 	private Set<Estudiante> estudiante = new HashSet<>();
-	
+
 	public Curso() {
 	}
 
 	public Curso(String cursoNombre, String cursoDuracion, String cursoDescripcion) {
 		super();
-		CursoNombre = cursoNombre;
+		this.cursoNombre = cursoNombre;
 		this.cursoDuracion = cursoDuracion;
 		this.cursoDescripcion = cursoDescripcion;
 	}
 
 	@Override
 	public String toString() {
-		return "Curso [cursoId=" + cursoId + ", CursoNombre=" + CursoNombre + ", cursoDuracion=" + cursoDuracion
+		return "Curso [cursoId=" + cursoId + ", CursoNombre=" + cursoNombre + ", cursoDuracion=" + cursoDuracion
 				+ ", cursoDescripcion=" + cursoDescripcion + "]";
 	}
-	
+
 	public void asignarEstudianteAlCurso(Estudiante estudiante) {
 		this.estudiante.add(estudiante);
 		estudiante.getCurso().add(this);
 	}
-	
+
 	public void retirarEstudianteDelCurso(Estudiante estudiante) {
 		this.estudiante.remove(estudiante);
 		estudiante.getCurso().add(this);

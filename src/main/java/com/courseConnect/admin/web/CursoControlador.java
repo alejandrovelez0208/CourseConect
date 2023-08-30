3c0998bca6754264e6296b1e9beb58efa389a713
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2023-08-29 19:37:52 -0500

    feat: se implementa controlador Delete y Update

--- src/main/java/com/courseConnect/admin/web/CursoControlador.java
+++ src/main/java/com/courseConnect/admin/web/CursoControlador.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.courseConnect.admin.entidad.Curso;
+import com.courseConnect.admin.entidad.Instructor;
 import com.courseConnect.admin.servicio.CursoServicio;
+import com.courseConnect.admin.servicio.InstructorServicio;
 
 @Controller
 @RequestMapping(value = "/cursos")
@@ -19,6 +21,9 @@ public class CursoControlador {
 	@Autowired
 	private CursoServicio cursoServicio;
 
+	@Autowired
+	private InstructorServicio instructorServicio;
+
 	public CursoControlador(CursoServicio cursoServicio) {
 		this.cursoServicio = cursoServicio;
 	}
@@ -26,8 +31,23 @@ public class CursoControlador {
 	@GetMapping(value = "/index")
 	public String cursos(Model model, @RequestParam(name = "keyword", defaultValue = "") String keyword) {
 		List<Curso> cursos = cursoServicio.encontrarCursosPorNombre(keyword);
-		model.addAttribute("listCursos",cursos);
+		model.addAttribute("listCursos", cursos);
 		model.addAttribute("keyword", keyword);
 		return "curso-views/cursos";
+	}
+
+	@GetMapping("/delete")
+	public String deleteCursos(Long cursoId, String keyword) {
+		cursoServicio.removerCurso(cursoId);
+		return "redirect:/cursos/index?keyword=" + keyword;
+	}
+
+	@GetMapping("/fromUpd")
+	public String updateCurso(Model model, Long courseId) {
+		Curso curso = cursoServicio.cargarCursoPorId(courseId);
+		List<Instructor> instructors = instructorServicio.fetchInstructor();
+		model.addAttribute("curso", curso);
+		model.addAttribute("listInstructors", instructors);
+		return "course-views/formUpdate";
 	}
 }

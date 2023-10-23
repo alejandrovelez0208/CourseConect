f316340789ba1256ded3471455a4ffcf8903232c
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2023-10-23 14:33:36 -0500

    feat: se hacen ajustes al controlador instructor y sus vistas

--- src/main/java/com/courseConnect/admin/web/InstructorControlador.java
+++ src/main/java/com/courseConnect/admin/web/InstructorControlador.java
@@ -5,13 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.courseConnect.admin.entidad.Instructor;
+import com.courseConnect.admin.entidad.Usuario;
 import com.courseConnect.admin.servicio.InstructorServicio;
+import com.courseConnect.admin.servicio.UsuarioServicio;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/instructores")
@@ -19,6 +24,9 @@ public class InstructorControlador {
 
 	@Autowired
 	private InstructorServicio instructorServicio;
+
+	@Autowired
+	private UsuarioServicio usuarioServicio;
 
 	@GetMapping(value = "/index")
 	public String instructor(Model model, @RequestParam(name = "keyword", defaultValue = "") String keyword) {
@@ -40,10 +48,28 @@ public class InstructorControlador {
 		model.addAttribute("instructor", instructor);
 		return "instructores-views/formUpdate";
 	}
-	
-	@PostMapping(value ="/update")
+
+	@PostMapping(value = "/update")
 	public String actualizar(Instructor instructores) {
 		instructorServicio.actualizarInstructor(instructores);
+		return "redirect:/instructores/index";
+	}
+
+	@GetMapping(value = "formCreate")
+	public String formInstructors(Model model) {
+		model.addAttribute("instructor", new Instructor());
+		return "instructores-views/formCreate";
+	}
+
+	@PostMapping(value = "/save")
+	public String save(@Valid Instructor instructor, BindingResult bindingResult) {
+		Usuario usuario = usuarioServicio.cargarUsuarioPorEmail(instructor.getUsuario().getEmail());
+		if (usuario != null)
+			bindingResult.rejectValue("usuario.email", null, "Hay una cuenta registrado con ese email");
+		if (bindingResult.hasErrors())
+			return "instructores-views/formCreate";
+		instructorServicio.crearInstructor(instructor.getNombres(), instructor.getApellidos(), instructor.getSummary(),
+				instructor.getUsuario().getEmail(), instructor.getUsuario().getContraseña());
 		return "redirect:/instructores/index";
 	}
 }
